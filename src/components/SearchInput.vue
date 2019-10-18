cd90d8020564ba4ed6e2d0af157761cf197d35a2
cholodymedia <cholodymedia@gmail.com>
2019-10-18 20:58:49 +0200

    Add basic styling

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="searchWrapper">
+    <input type="text" id="search" name="search" v-model="searchValue" @input="handleInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+};
+</script>
+
+<style lang="scss" scoped>
+  .searchWrapper {
+    margin-top: 50px;
+    width: 250px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+      height: 30px;
+      border: 0;
+      background: none;
+      border-bottom: 1px solid black;
+    }
+  }
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input type="text" id="search" name="search" v-model="searchValue" @input="handleInput"/>
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{item.data[0].description}}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim/>
+    <SearchInput/>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
   name: 'Search',
+  components: {
+    Claim,
+    SearchInput,
+  },
   data() {
     return {
       searchValue: '',
@@ -45,24 +44,14 @@ export default {
     margin: 0;
     padding: 30px;
     width: 100%;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
-
-  .search {
-    width: 250px;
-    display: flex;
-    flex-direction: column;
-
-    label {
-      font-family: Montserrat, sans-serif;
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
+    justify-content: center;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0%;
   }
 </style>
